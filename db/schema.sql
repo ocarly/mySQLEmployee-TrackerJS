6b353049573d62368e810004ca0d104ed1912c79
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-11-12 17:53:22 -0600

    adding tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,17 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE empDepartment (
-    id INT 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NULL
 );
 
-CREATE TABLE empPosition (
-
+CREATE TABLE empRole (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE empIdentity(
--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 require("dotenv").config();
+
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employeeTracker_db'
+});
